70c8041633b7693937d0f3bd8218a4b4d6e5a7c0
Opilane TTHK <opilane@tthk.ee>
2024-01-30 13:20:14 +0200

    stuff

--- TARpe22SciCalc/ViewModels/CalculatorPageViewModel.cs
+++ TARpe22SciCalc/ViewModels/CalculatorPageViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ViewModels
+﻿namespace ViewModels
 {
+    [INotifyPropertyChanged]
     internal partial class CalculatorPageViewModel
     {
+        [ObservableProperty]
+        private string inputText = string.Empty;
+
+        [ObservableProperty]
+        private string calculatedResult = "0";
+
+        private bool isSciOpWaiting = false;
     }
 }
